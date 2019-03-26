d7226654aab3a8ddbe0da4e339552802e5cb9ccc
ttSpeaker <js.serruya@gmail.com>
2019-03-26 17:03:00 -0300

    Added docker-compose file for node with mongodb

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var express = require("express");
+const MongoClient = require('mongodb').MongoClient
+//var bodyParser = require("body-parser");
 var app = express();
 
-// var MongoClient= require('mongodb').MongoClient
-// var mongoUrl = process.env.MONGO_URL ||  'mongodb://db:27017/test';
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connection URL
+const mongoUrl = process.env.MONGO_URL //|| 'mongodb://db:27017/test';
 
 app.get("/", function(req, res) {
   res.status(200).send("Hello JS Arg Off-Topic!");
-  // MongoClient.connect(mongoUrl,function (err,db){
-  //   if (err){
-  //     res.writeHead(500,)
-  //   }
-  // })
+});
+
+app.get("/db", function(req, res) {
+  MongoClient.connect(mongoUrl, (err, db) => {
+    if (err) {
+      res.status(500).send('Error connecting to MongoDB: ' + err);
+    } else {
+      res.status(200).send('Connected succesfully  MongoDB!');
+      db.close();
+    }
+  });
 });
 
 var port = "8080";
